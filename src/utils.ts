2aded8c3805f22eb413c89a20839f80a83b8f853
Viacheslav Dobromyslov <viacheslav@dobromyslov.ru>
2021-07-07 07:40:36 +0300

    Restored convertToArray function from @dobromyslov/core-utils.

--- src/utils.ts
+++ src/utils.ts
@@ -51,3 +51,21 @@ export function parseCounterpartyXmlData(xmlData: any): Counterparty {
 
   return counterparty;
 }
+
+/**
+ * @deprecated use from package @dobromyslov/core-utils
+ * Converts a single value to the array containing the single value.
+ * If the value is undefined then returns an empty array.
+ * @param arrayOrSingleValue A single value or an array.
+ */
+export function convertToArray<T>(arrayOrSingleValue: T): T[] {
+  if (arrayOrSingleValue === undefined) {
+    return [];
+  }
+
+  if (!Array.isArray(arrayOrSingleValue)) {
+    return [arrayOrSingleValue];
+  }
+
+  return arrayOrSingleValue;
+}
